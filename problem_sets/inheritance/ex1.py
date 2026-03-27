5d526ed73d6f95bdd2546935e11eec35c1c39532
Jacob Morrin <jacob.morrin@gmail.com>
2026-03-26 19:23:54 -0600

    More problems

---
+++ problem_sets/inheritance/ex1.py
@@ -0,0 +1,17 @@
+class Dog:
+    def speak(self):
+        return 'bark!'
+
+    def sleep(self):
+        return 'sleeping!'
+
+class Bulldog(Dog):
+    def sleep(self):
+        return 'snoring!'
+    
+teddy = Dog()
+print(teddy.speak())      # bark!
+print(teddy.sleep())       # sleeping!
+karl = Bulldog()
+print(karl.speak())       # bark!
+print(karl.sleep())        # snoring!
